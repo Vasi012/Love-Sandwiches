19e92311ba9791ec9b90b97c21ae31432ec31592
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2022-05-06 11:25:51 +0000

    initial commit

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+SHEET = GSPREAD_CLIENT.open('LoveSandwichies')
 
 sales = SHEET.worksheet('sales')
 

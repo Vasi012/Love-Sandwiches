6604b1d19dabbd5ce58618a04dd6fbe7d4d9f950
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2022-05-06 11:50:43 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('LoveSandwichies')
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 sales = SHEET.worksheet('sales')
 
